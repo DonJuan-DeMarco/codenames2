471b54ba925f65a92623635a153a976ba3aa2435
Vladyslav Yamko <vladyslav.yamko@jito.dev>
2023-09-17 01:30:37 +0300

    game

--- src/Pages/Card.jsx
+++ src/Pages/Card.jsx
@@ -1,4 +1,6 @@
+import jsPDF from 'jspdf';
 import word_list from '../words';
+import html2canvas from 'html2canvas';
 
 function listToMatrix(list, elementsPerSubArray) {
 	var matrix = [],
@@ -17,11 +19,47 @@ function listToMatrix(list, elementsPerSubArray) {
 	return matrix;
 }
 const wordList = listToMatrix(word_list, 16);
+console.log(word_list);
 function Card() {
+	const options = {
+		scale: 11,
+		useCORS: true,
+	};
+
+	const createPDF = async () => {
+		console.log('startedPDF');
+		const pdf = new jsPDF('landscapee', 'cm', [6.6, 4.4]);
+		word_list.length = 10;
+		const dd = word_list.map(async (word, idx) => {
+			console.log('#' + idx + ' working on ' + word);
+			const data = await html2canvas(
+				document.querySelector(`#pdf_${idx}`),
+				options
+			);
+			console.log(data);
+			const img = data.toDataURL(`image${idx + 1}/png`);
+			const imgProperties = pdf.getImageProperties(img);
+			const pdfWidth = pdf.internal.pageSize.getWidth();
+			const pdfHeight =
+				(imgProperties.height * pdfWidth) / imgProperties.width;
+			pdf.addPage([pdfWidth, pdfHeight], 'l');
+			pdf.addImage(img, 'PNG', 0, 0, pdfWidth, pdfHeight);
+			console.log(' finished on ' + word);
+		});
+		console.log('preprom');
+		if (await Promise.all(dd)) {
+			console.log('PROM');
+			pdf.save(`card_2.pdf`);
+		}
+	};
+
 	return (
 		<>
-			<p className='counter'>Pages Count: {wordList.length}</p>
-			<p className='counter'>Words Cunt: {word_list.length}</p>
+			<p className='counter'>Сторінок: {wordList.length}</p>
+			<p className='counter'>Кількість слів: {word_list.length}</p>
+			<button onClick={createPDF} type='button'>
+				Download
+			</button>
 			{/* <div className='wrapper-words'> */}
 			{wordList.map((words, pageKey) => (
 				// <div
@@ -30,7 +68,11 @@ function Card() {
 				// >
 				<div key={pageKey} className='keys-wrapper'>
 					{words.map((word, index) => (
-						<div className='word-card' key={index}>
+						<div
+							className='word-card'
+							key={index}
+							id={`pdf_${pageKey * index + index}`}
+						>
 							{/* {(word.toLowerCase() === 'королева' ||
 								word.toLowerCase() === 'міледі' ||
 								word.toLowerCase() === 'сонік' ||
@@ -39,6 +81,9 @@ function Card() {
 							)} */}
 
 							<p className='word mirrored'>{word}</p>
+							{/* <div className='main-field-inner-border' />
+							<div className='main-field-outer-border' />
+							<div className='pseudo-main-field' /> */}
 							<div className='main-field'>
 								<p className='word main-word'>{word}</p>
 							</div>
@@ -47,7 +92,7 @@ function Card() {
 				</div>
 			))}
 			<p className='counter'>Pages Count: {wordList.length}</p>
-			<p className='counter'>Words Cunt: {word_list.length}</p>
+			<p className='counter'>Words Count: {word_list.length}</p>
 		</>
 	);
 }
